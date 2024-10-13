2a25597b46a7eb30051909e8eca780c9780831d0
Lawer <lawerpls@gmail.com>
2024-10-13 16:13:02 -0300

    Eastergg Adicionado

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ function toggleMode() {
 
   html.classList.toggle("light")
 }
+
+let clickCount = 0
+
+const profileName = document.getElementById("profile-name")
+const modal = document.getElementById("easteregg-modal")
+const closeModalBtn = document.querySelector(".close-btn")
+
+profileName.addEventListener("click", function () {
+  clickCount++
+
+  if (clickCount === 3) {
+    modal.style.display = "block"
+    clickCount = 0
+  }
+})
+
+closeModalBtn.addEventListener("click", function () {
+  modal.style.display = "none"
+})
+
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none"
+  }
+})
